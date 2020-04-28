99b5dfd91f9f3daa78c7620b361c35314bf9a2d3
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-28 13:29:49 +0500

    annotation AfterEach added

--- src/test/java/ru/netology/web/TestCart.java
+++ src/test/java/ru/netology/web/TestCart.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,8 +25,14 @@ public class TestCart {
         driver = new ChromeDriver();
     }
 
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver=null;
+    }
+
     @Test
-    void shouldTestV1() {
+    void shouldTestVCorrectValues() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Василий");
         driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79270000000");
@@ -55,5 +62,27 @@ public class TestCart {
         driver.findElement(By.cssSelector(".button__text")).submit();
         String text = driver.findElement(By.cssSelector("[data-test-id]")).getText();
         assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+
+    @Test
+    void shouldTestWithOneNumber() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Васильев Александр");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("7");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String text = driver.findElement(By.cssSelector(".input__sub")).getText();
+        assertEquals("Укажите точно как в паспорте", text);
+    }
+
+    @Test
+    void shouldTestWithEmptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("span[class='checkbox__box']")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String text = driver.findElement(By.cssSelector(".input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text);
     }
 }
