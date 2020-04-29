5ccd86f2642d763e0d60544af0fdc04bd7eedd44
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-30 01:03:47 +0500

    webdriver added

--- src/test/java/ru/netology/web/TestCart.java
+++ src/test/java/ru/netology/web/TestCart.java
@@ -1,28 +1,20 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.sql.Driver;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SeleniumExtension.class)
 public class TestCart {
-    private WebDriver driver;
 
-    public void setupTest() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
+    private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
