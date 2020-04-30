d4eccc6a17a36397050f1f4b333cf5031d49c382
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-30 13:58:15 +0500

    lin driver added

--- src/test/java/ru/netology/web/TestCart.java
+++ src/test/java/ru/netology/web/TestCart.java
@@ -1,43 +1,39 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.sql.Driver;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SeleniumExtension.class)
 public class TestCart {
     private WebDriver driver;
 
-    public void setupTest() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
-    }
+//    @BeforeAll
+//    static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+//    }
 
     @BeforeEach
     void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    public void setupTest() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    void tearDown(){
+    public void tearDown(){
         driver.quit();
-        driver=null;
+        driver = null;
     }
 
     @Test
