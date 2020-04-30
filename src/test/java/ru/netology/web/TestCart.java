4940b848099c42291ddbd02a33b7df9ce0ac3db7
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-30 14:09:36 +0500

    lin driver added

--- src/test/java/ru/netology/web/TestCart.java
+++ src/test/java/ru/netology/web/TestCart.java
@@ -10,23 +10,28 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SeleniumExtension.class)
 public class TestCart {
     private WebDriver driver;
 
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
-//    }
+    public void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pepper\\IdeaProjects\\TestCart\\driver\\win\\chromedriver.exe");
+    }
 
     @BeforeEach
     void setUp() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    public void setupTest() {
         driver = new ChromeDriver();
     }
 
